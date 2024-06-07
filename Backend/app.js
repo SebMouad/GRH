02de8a6a64e43758eb5f8df3873568936caadc8f
SebMouad <132716054+SebMouad@users.noreply.github.com>
2024-06-07 22:12:04 +0100

    Update API CORS

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const employeeRoutes = require('./routes/employees');
 const leaveRoutes = require('./routes/leaves');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
